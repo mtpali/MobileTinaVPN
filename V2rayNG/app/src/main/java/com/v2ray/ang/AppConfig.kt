7a5b7caaa11aca6b8412dcee9554b1e300001434
VPN963 <120357728+VPN963@users.noreply.github.com>
2026-08-13 12:55:05 +0330

    Remove obsolete WebDAV backup constants

--- V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -10,9 +10,6 @@ object AppConfig {
     /** Directory names used in the app's file system. */
     const val DIR_ASSETS = "assets"
 
-    const val WEBDAV_BACKUP_DIR = "backups"
-    const val WEBDAV_BACKUP_FILE_NAME = "backup_ng.zip"
-
     /** Legacy configuration keys. */
     const val ANG_CONFIG = "ang_config"
 
